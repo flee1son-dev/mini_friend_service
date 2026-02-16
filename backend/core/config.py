31247f7603fdae4819b27d87bc3c698460348752
Nikita Shmidt <Flee1son@gmail.com>
2026-02-16 21:17:18 +0300

    Add comments to core/config.py

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DEBUG: bool = False
     
+    #jwt settings
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
@@ -10,6 +11,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_TYPE: str = "access"
     REFRESH_TOKEN_TYPE: str = "refresh"
 
+    #database settings
     DATABASE_URL: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str

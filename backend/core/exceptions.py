23eac58d8f0c43bf6a400b2d9f436d27530b1dff
Nikita Shmidt <Flee1son@gmail.com>
2026-02-17 21:20:26 +0300

    Create exceptions.py: add auth, user, friendship, validaton exception

---
+++ backend/core/exceptions.py
@@ -0,0 +1,94 @@
+from fastapi import HTTPException, status
+
+
+#Base exception
+class AppException(HTTPException):
+    """application exception"""
+    def __init__(self, status_code: int, detail: str):
+        super().__init__(status_code=status_code, detail=detail)
+
+
+#Auth exception
+class InvalidCredentials(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+    )
+
+class CredentialsException(AppException):
+    """token error"""
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials"
+        )
+
+class UserInactive(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Account deleted"
+        )
+
+
+#User exceptions
+class UserNotFound(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+class UserForbidden(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not allowed to perform this action"
+        )
+
+class UserEmailAlreadyExists(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with this email already exists"
+        )
+
+
+#Validation exception
+class ValidationError(AppException):
+    def __init__(self, detail: str):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=detail
+        )
+
+
+#Friendship exception
+class FriendShipNotFound(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Friendship not found"
+        )
+
+class FriendRequestAlreadyExists(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Friend request already exists"
+        )
+
+class FriendRequestAlreadyAccepted(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="The friend request has already been accepted"
+        )
+
+class FriendRequestAlreadyRejected(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="The friend request has already been rejected"
+        )

9e1fc2f0a7e26b7e03860db74f44a9060906559e
Nikita Shmidt <Flee1son@gmail.com>
2026-03-15 17:39:16 +0300

    Fix core -fix model config from config.py -fix JWT TOKENS from security.py

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DEBUG: bool = False
@@ -18,10 +18,10 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     POSTGRES_HOST: str
 
-    model_config = {
-        "env-file": ".env",
-        "env_file_encoding": "utf-8"
-    }
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
 
 
 settings = Settings()

d461365dac80329004d1926fecf5431d72dee53c
Nikita Shmidt <Flee1son@gmail.com>
2026-03-17 10:43:01 +0300

    Created a test database from in-memory SQLite

---
+++ tests/test_base.py
@@ -0,0 +1,42 @@
+import unittest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from backend.core.database import Base, get_db
+from backend.main import app
+
+
+"""Setting up SQLite in-memory"""
+DATABASE_URL = "sqlite:///:memory:"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+TestingSessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+"""Base class for tests"""
+class BaseTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """create all tables before testing"""
+        Base.metadata.create_all(engine)
+    
+    @classmethod
+    def tearDownClass(cls):
+        """delete all tables after testing"""
+        Base.metadata.drop_all(engine)
+
+
+    def setUp(self):
+        """Create a new session before each test"""
+        self.db = TestingSessionLocal()
+
+    def tearDown(self):
+        """Roll back changes after each test"""
+        self.db.rollback()
+        self.db.close()
+
+        app.dependency_overrides.clear()
+
+
